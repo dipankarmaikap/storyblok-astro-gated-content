6c143ae59be8226d258fe099f30286d93ea18f3c
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2024-07-17 16:50:59 +0530

    Signin signup script code optimized

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,22 @@
 export const isPreview = import.meta.env.STORYBLOK_IS_PREVIEW === "yes";
+
+export function handelFormSubmit(formName: string) {
+  const errorMessageElement = document.getElementById("form-error")!;
+  const form = document.getElementById(formName);
+
+  form?.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    errorMessageElement.innerText = "";
+    const formElement = e.target as HTMLFormElement;
+    const response = await fetch(formElement.action, {
+      method: formElement.method,
+      body: new FormData(formElement),
+    });
+    const data = await response.json();
+    if (data.sucess) {
+      window.location.href = "/";
+    } else {
+      errorMessageElement.innerText = data.error;
+    }
+  });
+}
